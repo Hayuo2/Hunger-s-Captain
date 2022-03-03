c35a3832abd7702ae32acbc79646ee4c68fc4fd0
rajitechis <rajit@techis.io>
2022-03-03 08:10:46 +0530

    added html files

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ImgLogo from "../../assets/img/logo.svg";
 
-const Footer = ({ price, showCartList, setShowCartList }) => {
+const Footer = ({ price }) => {
   return (
     <footer>
       <div class="subtotal">
         <span class="subtotal-test">Subtotal:</span>
         <span class="subtotal-price">${price}</span>
       </div>
-      
-       
-      
     </footer>
   );
 };
